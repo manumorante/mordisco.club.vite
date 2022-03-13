d17c71c2537fe470299020cecdcd3fa1d40e3e88
Manu Morante <manu@estadologico.com>
2022-03-13 18:42:42 +0100

    CL UB

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import coverImg from "./img/mordisco-club-people.jpg"
 function App() {
   return (
     <div className="App w-screen h-screen">
-      <div className="container max-w-3xl mx-auto py-10 px-8 relative z-20">
+      <div className="relative z-20 w-full h-full flex items-center justify-center">
         <div className="Logo flex gap-2 items-center text-xl sm:text-3xl text-white tracking-tight">
           <span>Mordisco</span>
-          <span className="rotate-45 w-9 h-9 bg-white ">
-            <div className="-rotate-45 flex justify-center items-center w-full h-full text-gray-600 text-center text-xs">
-              Club
+          <span className="rotate-45 w-8 h-8 bg-white ">
+            <div className="-rotate-45 flex justify-center items-center w-full h-full text-gray-600 text-center text-xs leading-none">
+              {`CL
+              UB`}
             </div>
           </span>
         </div>
