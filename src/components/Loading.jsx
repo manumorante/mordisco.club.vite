dcf1a2129e82700427f541c1cb95292c7e94a157
Manu Morante <manu@estadologico.com>
2022-05-20 14:34:24 +0200

    Loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='absolute inset-0 m-auto w-12 h-12'>
+      <div className='loading'></div>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 module.exports = {
   content: ['./src/**/*.{html,jsx}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'wiggle-r':
+          'wiggle-r 1s cubic-bezier(0.5, 0.8, 0.5, 0.2) infinite forwards',
+        'wiggle-l':
+          'wiggle-l 1s cubic-bezier(0.5, 0.8, 0.5, 0.2) infinite forwards',
+      },
+      keyframes: {
+        'wiggle-l': {
+          '0%, 15%, 25%, 100%': { transform: 'translate(0, 0)' },
+          '17%': { transform: 'translate(-20%, 0)' },
+          '22%': { transform: 'translate(30%, 0)' },
+          '24%': { transform: 'translate(-10%, 0)' },
+        },
+        'wiggle-r': {
+          '0%, 25%, 35%, 100%': { transform: 'translate(0, 0)' },
+          '27%': { transform: 'translate(20%, 0)' },
+          '32%': { transform: 'translate(-30%, 0)' },
+          '34%': { transform: 'translate(10%, 0)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
