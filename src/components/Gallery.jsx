216598f6a064a6e484e35dfcac46fedbed802931
Manu Morante <manu@estadologico.com>
2022-05-26 22:25:00 +0200

    Use index of array vs photo.index

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 import Photo from './Photo'
 
 export default function Gallery({ album, dispatch }) {
-
   return (
     <div className='[Gallery] masonry'>
       {album.photos.map((photo, index) => {
         return (
           <div
-            key={`g-${photo}`}
+            key={`g-${index}`}
             className='masonry__item'
             style={{
               '--img-w': photo.width,
               '--img-h': photo.height,
             }}>
-            <Photo path={`${album.path}/s`} photo={photo} index={index} dispatch={dispatch} />
+            <Photo
+              path={`${album.path}/s`}
+              photo={photo}
+              index={index}
+              dispatch={dispatch}
+            />
           </div>
         )
       })}
