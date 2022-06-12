a2452d7cd62f74976ca7ccb868a0ab3e98527f10
Manu Morante <manu@estadologico.com>
2022-06-12 15:49:03 +0200

    Slideshow only when over

--- src/components/Gallery/AlbumCover.jsx
+++ src/components/Gallery/AlbumCover.jsx
@@ -4,50 +4,69 @@ export default function AlbumCover({ album }) {
   const PHOTOS = album.photos
   const TOTAL = PHOTOS.length
 
+  const [isHover, setIsHover] = useState(false)
   const [turn, setTurn] = useState(0)
 
+  // To control first time and show quickly the next cover
+  const [first, setFirst] = useState(true)
+
   const [pos, setPos] = useState(0)
-  const [posA, setPosA] = useState(0)
-  const [posB, setPosB] = useState(1)
+  const [srcA, setSrcA] = useState(PHOTOS[0].small)
+  const [srcB, setSrcB] = useState(PHOTOS[1].small)
 
   const [aClass, setAClass] = useState('opacity-100')
   const [bClass, setBClass] = useState('opacity-0')
 
-  const slideshowInterval = 2000
-  const transitionDuration = 2000
-  const transitionDurationClass = 'duration-2000'
+  const slideshowInterval = 1000
+  const transitionDuration = 1000
+  const transitionDurationClass = 'duration-1000'
+
+  const handleMouseOut = () => {
+    setIsHover(false)
+  }
+
+  const handleMouseOver = () => {
+    setIsHover(true)
+    setFirst(true)
+  }
 
   // Rotate turns
   useEffect(() => {
+    if (!isHover) return false
+
     const active = 'z-10 opacity-100'
     const inactive = 'z-0 opacity-0'
 
-    const timer = setTimeout(() => {
-      setAClass(turn === 0 ? active : inactive)
-      setBClass(turn === 1 ? active : inactive)
+    const timer = setTimeout(
+      () => {
+        setAClass(turn === 0 ? active : inactive)
+        setBClass(turn === 1 ? active : inactive)
 
-      const newPos = pos === TOTAL - 1 ? 0 : pos + 1
-      setPos(newPos)
+        const newPos = pos === TOTAL - 1 ? 0 : pos + 1
+        const newSrc = PHOTOS[newPos].small
+        setPos(newPos)
 
-      // Wait for the transition to finish and preload the next image
-      setTimeout(() => {
-        console.log('newPos', newPos)
-        turn === 0 ? setPosB(newPos) : setPosA(newPos)
-      }, transitionDuration)
+        // Wait for the transition to finish and preload the next image
+        turn === 0
+          ? setTimeout(() => setSrcB(newSrc), transitionDuration)
+          : setTimeout(() => setSrcA(newSrc), transitionDuration)
 
-      setTurn((turn) => (turn === 0 ? 1 : 0))
-    }, slideshowInterval)
+        setTurn((turn) => (turn === 0 ? 1 : 0))
+        setFirst(false)
+      },
+      first ? 0 : slideshowInterval
+    )
     return () => clearTimeout(timer)
-  }, [turn])
+  }, [turn, isHover])
 
   return (
-    <div className='AlbumCover'>
+    <div className='AlbumCover' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
       <div className={`transition-opacity ${transitionDurationClass} ${aClass}`}>
-        <img className='AlbumCover__img' src={PHOTOS[posA].small} alt='Album' />
+        <img className='AlbumCover__img' src={srcA} alt='Album' />
       </div>
 
       <div className={`transition-opacity ${transitionDurationClass} ${bClass}`}>
-        <img className='AlbumCover__img' src={PHOTOS[posB].small} alt='Album' />
+        <img className='AlbumCover__img' src={srcB} alt='Album' />
       </div>
     </div>
   )
